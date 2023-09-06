7d82b48319e0a4a4f1de114e75c50ec648783511
Daudu Arunegbode charles <blaqwayschannel@gmail.com>
2023-09-06 16:35:48 +0100

    basic server set up with express js

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const dotenv = require('dotenv').config();
+
+const app = express();
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+
+
+
+
+
+
+
+
+app.listen(process.env.PORT, ()=>{
+    console.log(`server is listening on port ${PORT} `)
+})
